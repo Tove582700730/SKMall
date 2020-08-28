a53bf373d08640d5ecff24366178ec3fd1aaf740
Tove582700730 <582700730@qq.com>
2020-08-28 21:06:12 +0800

    Merge branch 'master' of https://github.com/Tove582700730/SKmall
    # Conflicts:
    #	src/main/java/com/qianfeng/controller/test.java

---
+++ src/main/java/test/test.java
@@ -0,0 +1,24 @@
+package test;
+
+import  java.util.*;
+public class test {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+      ArrayList<Integer> list = new ArrayList<>();
+      char[] arr = {'a','b','v'};
+      String  b = Arrays.toString(arr);
+        System.out.println(arr.length);
+        System.out.println(b);
+        System.out.println(b.length());
+        String c ="abv";
+        System.out.println(c);
+        System.out.println(c.length());
+
+        int[] brr = {569,8};
+        String d = Arrays.toString(brr);
+        System.out.println(brr.length);
+        System.out.println(d);
+        System.out.println(d.length());
+    }
+
+}
